67d24db18fd1d11c9f460397ee00530a6f0bec06
StephFallesen <stephanie1507@live.dk>
2022-05-24 13:14:04 +0200

    poof

--- sub.js
+++ sub.js
@@ -1,12 +1,12 @@
 
 document.getElementById("Overly").addEventListener("click", function(){
- var e =document.getElementsByClassName("modalbox");
+ let e =document.getElementsByClassName("modalbox");
 
         e[0].style.display = 'block';
 
 })	;
 document.getElementById("close").addEventListener("click", function(){
-   var e =document.getElementsByClassName("modalbox");
+   let e =document.getElementsByClassName("modalbox");
  e[0].style.display= 'none';
 });
 
