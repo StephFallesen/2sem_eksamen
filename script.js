db1bd11a6ba79726ebd680c82aac055b34fbed17
StephFallesen <stephanie1507@live.dk>
2022-05-24 11:53:56 +0200

    updates

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ function closeNav() {
 }
 
 
-
-console.log(countDownDate);
 // Countdown
 // Set the date we're counting down to
 let countDownDate = new Date("June 31, 2022 12:00:00").getTime();
@@ -31,13 +29,13 @@ let x = setInterval(function() {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + " DAYS : " + hours + " HOURS : "
+  // Display the result in the element with id="count"
+  document.getElementById("count").innerHTML = days + " DAYS : " + hours + " HOURS : "
   + minutes + " MINUTES : " + seconds + " SEKUNDS";
 
   // Kontrolstruktur hvis countdown er nået den ønskede dato:
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "UPDATE COMPLETED";
+    document.getElementById("count").innerHTML = "UPDATE COMPLETED";
   }
 }, 1000);
