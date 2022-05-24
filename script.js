976d0e2e6bf71aad3039956b76464ef7c589eeb7
StephFallesen <stephanie1507@live.dk>
2022-05-24 08:56:55 +0200

    update steph

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 //menu
 function openNav() {
   document.getElementById("subscriptoNav").style.width = "100%";
-  document.getElementById("hamburger").style.marginLeft = "0";
+  document.getElementById("hamburger").style.marginLeft = "0px";
 }
 
 function closeNav() {
   document.getElementById("subscriptoNav").style.width = "0";
-  document.getElementById("hamburger").style.marginLeft= "0";
+  document.getElementById("hamburger").style.marginLeft= "20px";
 }
+
+//Dropdown Category
+function dropDown() {
+  document.getElementById('cte').classList.toggle("show")
+}
+let categoryArray = new Array("Category", "Development", "Design", "Management", "Marketing");
+
+  let dropdown = document.getElementById('cte');
+
+  for (let i = 0; i < categoryArray.length; ++i) {
+    dropdown[dropdown.length] = new Option(categoryArray[i])
+  }
+
+  //Dropdown Payment
+  function moneyFunc() {
+    document.getElementById('pay').classList.toggle("show")
+  }
+  let paymentArray = new Array("Paymentplan", "Monthly", "Yearly");
+
+    let money = document.getElementById('pay');
+
+    for (let i = 0; i < paymentArray.length; ++i) {
+      money[money.length] = new Option(paymentArray[i])
+    }
 
 
 // Countdown
