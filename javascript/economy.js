fabf49c14de08b06f4c4e5dcd8a8d924bcfa9b99
Anne-Sofie <95890841+Fidolicious@users.noreply.github.com>
2022-05-25 14:08:13 +0200

    search

--- javascript/economy.js
+++ javascript/economy.js
@@ -25,4 +25,18 @@ function myFunction() {
     let price7 = document.getElementById("input-total7").value;
 	let total = +price + +price1 + +price2 + +price3 + +price4 + +price5 + +price6 + +price7;
 	document.getElementById("total-price").innerHTML = total;
+
+}
+
+function myFunction1() {
+	let prices = document.getElementById("in-total").value;
+    let prices1 = document.getElementById("in-total1").value;
+    let prices2 = document.getElementById("in-total2").value;
+    let prices3 = document.getElementById("in-total3").value;
+    let prices4 = document.getElementById("in-total4").value;
+    let prices5 = document.getElementById("in-total5").value;
+    let prices6 = document.getElementById("in-total6").value;
+    let prices7 = document.getElementById("in-total7").value;
+	let total1 = +prices + +prices1 + +prices2 + +prices3 + +prices4 + +prices5 + +prices6 + +prices7;
+	document.getElementById("total-prices").innerHTML = total1;
 }
