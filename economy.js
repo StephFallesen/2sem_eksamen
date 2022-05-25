8581c99d138ffa7372c76312518a295491f06a86
Anne-Sofie <95890841+Fidolicious@users.noreply.github.com>
2022-05-25 09:33:47 +0200

    economy update

--- economy.js
+++ economy.js
@@ -1,6 +1,6 @@
-var x = document.getElementById("monthly");
-var y = document.getElementById("yearly");
-var btn = document.getElementById("btn");
+let x = document.getElementById("monthly");
+let y = document.getElementById("yearly");
+let btn = document.getElementById("btn");
 
 function yearly() {
     x.style.left = "-1200px";
@@ -14,5 +14,15 @@ function monthly() {
     btn.style.left = "0";
 }
 
-
-  
+function myFunction() {
+	let price = document.getElementById("input-total").value;
+    let price1 = document.getElementById("input-total1").value;
+    let price2 = document.getElementById("input-total2").value;
+    let price3 = document.getElementById("input-total3").value;
+    let price4 = document.getElementById("input-total4").value;
+    let price5 = document.getElementById("input-total5").value;
+    let price6 = document.getElementById("input-total6").value;
+    let price7 = document.getElementById("input-total7").value;
+	let total = +price + +price1 + +price2 + +price3 + +price4 + +price5 + +price6 + +price7;
+	document.getElementById("total-price").innerHTML = total;
+}
