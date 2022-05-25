f0bc4104189e8dc825a85afd93242300e71a45be
StephFallesen <stephanie1507@live.dk>
2022-05-25 22:17:08 +0200

    styleguide update

--- style.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "100%";
-  document.getElementById("ham").style.marginLeft = "0px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("ham").style.marginLeft= "20px";
-}
