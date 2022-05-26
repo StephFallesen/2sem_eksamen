620eb3a951f7046019667ea5e11a885d9ee3f7c9
StephFallesen <stephanie1507@live.dk>
2022-05-26 20:13:22 +0200

    update

--- sub.js
+++ sub.js
@@ -17,30 +17,32 @@ document.getElementById("Overly").addEventListener("click", function(){
 
 })	;
 document.getElementById("close").addEventListener("click", function(){
-   let e =document.getElementsByClassName("modalbox");
- e[0].style.display= 'none';
-});
+     let e =document.getElementsByClassName("modalbox");
+     e[0].style.display= 'none';
+  });
+}
+
 
 //Dropdown Category
 function dropDown() {
-  document.getElementById('cte').classList.toggle("show")
+  document.getElementById('cte').classList.toggle("show");
 }
 let categoryArray = new Array("Category", "Development", "Design", "Management", "Marketing");
 
   let dropdown = document.getElementById('cte');
 
   for (let i = 0; i < categoryArray.length; ++i) {
-    dropdown[dropdown.length] = new Option(categoryArray[i])
+    dropdown[dropdown.length] = new Option(categoryArray[i]);
   }
 
   //Dropdown Payment
   function moneyFunc() {
-    document.getElementById('pay').classList.toggle("show")
+    document.getElementById('pay').classList.toggle("show");
   }
   let paymentArray = new Array("Paymentplan", "Monthly", "Yearly");
 
     let money = document.getElementById('pay');
 
     for (let i = 0; i < paymentArray.length; ++i) {
-      money[money.length] = new Option(paymentArray[i])
+      money[money.length] = new Option(paymentArray[i]);
     }
