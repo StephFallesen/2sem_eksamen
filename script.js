5233708929fba193ec86620a944cd5954c2a6bd0
StephFallesen <stephanie1507@live.dk>
2022-05-26 18:30:20 +0200

    js

---
+++ script.js
@@ -0,0 +1,40 @@
+//menu
+function openNav() {
+  document.getElementById("subscriptoNav").style.width = "100%";
+  document.getElementById("hamburger").style.marginLeft = "0px";
+}
+
+function closeNav() {
+  document.getElementById("subscriptoNav").style.width = "0";
+  document.getElementById("hamburger").style.marginLeft= "20px";
+}
+
+// Countdown
+// Set the date we're counting down to
+let countDownDate = new Date("June 31, 2022 12:00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("count").innerHTML = days + " DAYS : " + hours + " HOURS : "
+  + minutes + " MINUTES : " + seconds + " SEKUNDS";
+
+  // Kontrolstruktur hvis countdown er nået den ønskede dato:
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("count").innerHTML = "UPDATE COMPLETED";
+  }
+}, 1000);
