b3e6bd0e2d2842524762d65b8e761c246ac99527
Caro3738 <heinmann3@gmail.com>
2022-05-29 10:55:39 +0200

    Caro's javascript

--- javascript/cloud.js
+++ javascript/cloud.js
@@ -8,3 +8,34 @@ function closeNav() {
   document.getElementById("subscriptoNav").style.width = "0";
   document.getElementById("hamburger").style.marginLeft= "20px";
 }
+
+
+//* text animation
+const textEl = document.querySelector('.text-anim');
+const textElStr = textEl.innerText;
+const textCharArr = textElStr.split('');
+
+textEl.innerText = '';
+
+for (let i=0; i<textCharArr.length; i++){
+  textEl.innerHTML += '<span>' + textCharArr[i] + '</span>';
+}
+
+let count = 0;
+let timer = setInterval(startAnimation, 50);
+
+function startAnimation(){
+  const span = textEl.querySelectorAll('span')[count];
+  span.classList.add('fade');
+  count++;
+
+  if (count === textCharArr.length){
+    stopAnimation();
+    return;
+  }
+}
+
+function stopAnimation(){
+  clearInterval(timer);
+  timer = null;
+}
